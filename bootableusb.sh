5d3fee1ed19548172530b7f9b149bb7685585ddf
xxcosita3czxx <kokosove18@gmail.com>
2025-12-13 12:33:32 +0100

    commenitng

--- bootableusb.sh
+++ bootableusb.sh
@@ -8,6 +8,9 @@
 #Warning: Insert One USB at a time only. If you have any other USB either remove them from the system or change the script accordingly. Check size with lsblk (uncommment line 10 & 11).
 
 
+# Note by cosita:
+#id rather not use this, there is no safety checks, no verification of target device, it can kill your own system
+
 echo "Welcome to Bootable USB Maker!"
 echo "We are making ALG Bootable USB, please wait..."
 echo "                               "
