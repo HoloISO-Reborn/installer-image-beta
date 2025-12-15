45f4846756a2e7d4d55359a2b36cedd050ceac60
xxcosita3czxx <kokosove18@gmail.com>
2025-12-15 14:00:26 +0100

    better builder

--- build.sh
+++ build.sh
@@ -2,14 +2,86 @@
 
 # ./build.sh 
 
-INSTALLDIR="${BUILDDIR}/holoiso-installer/${BRANCH}"
-WORKDIR="/tmp/$(date +%Y%m%d.%H%M.%S)"
-mkdir -p $INSTALLDIR
-rm -rf ${SCRIPTPATH}/airootfs/etc/holoinstall/*zst
-source ${BUILDDIR}/holoiso-images/latest_$BRANCH.releasemeta
-mkdir -p ${SCRIPTPATH}/airootfs/etc/holoinstall
-cp ${BUILDDIR}/holoiso-images/${IMAGEFILE}.img.zst ${SCRIPTPATH}/airootfs/etc/holoinstall
+#VARS
+_SCRIPT=$(realpath "$0")
+script_path=$(dirname "$_SCRIPT")
+work="/tmp/$(date +%Y%m%d.%H%M.%S)"
+
+offline=true
+clean=false
+
+# Parse flags if provided
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --offline)
+            offline=true
+            shift
+            ;;
+        --online)
+            offline=false
+            shift
+            ;;
+        --clean)
+            clean=true
+            shift
+            ;;
+        --images)
+            holoiso_images_dir="$2"
+            if [[ -z "${holoiso_images_dir}" ]]; then
+                echo "Please provide a valid holoiso-images directory path after --images"
+                exit 1
+            fi
+            shift
+            shift
+            ;;
+        --branch)
+            BRANCH="$2"
+            if [[ -z "${BRANCH}" ]]; then
+                echo "Please provide a valid branch name after --branch"
+                exit 1
+            fi
+            shift
+            shift
+            ;;
+        --output-dir)
+            output_dir="$2"
+            shift
+            shift
+            ;;
+        *)
+        echo "Unknown flag: $1"
+        exit 1
+        ;;
+    esac
+done
+
+if [[ -z "${output_dir}" ]]; then
+    output_dir="out/${BRANCH}"
+fi
+# prep
+mkdir -p $output_dir
+rm -rf ${script_path}/airootfs/etc/holoinstall/*zst
+mkdir -p ${script_path}/airootfs/etc/holoinstall
+
+if [ "$offline" = true ]; then
+    if [[ -z "${holoiso_images_dir}" ]]; then
+        echo "Please provide holoiso-images directory path using --images"
+        exit 1
+    fi
+    source  ${holoiso_images_dir}/latest_$BRANCH.releasemeta
+    cp ${holoiso_images_dir}/${IMAGEFILE}.img.zst ${script_path}/airootfs/etc/holoinstall
+    echo "Building in offline mode."
+else
+    echo "Building in online mode. NOTE: Comming soon"
+fi
+
+# Sets the iso name in /profiledef.sh
 echo ${IMAGEFILE} > /tmp/currentcandidate
-sudo mkarchiso -v -w ${WORKDIR} -o ${INSTALLDIR} ${SCRIPTPATH}
-rm -rf ${SCRIPTPATH}/airootfs/etc/holoinstall/*zst
-rm -rf ${WORKDIR}
+
+# ACTUALL BUILD
+sudo mkarchiso -v -w ${work} -o ${output_dir} ${script_path}
+
+if [ "$clean" == true ]; then
+    rm -rf ${script_path}/airootfs/etc/holoinstall/*zst
+    rm -rf ${work}
+fi
--- getlatestbuild.sh
+++ getlatestbuild.sh
@@ -9,7 +9,8 @@ DOWNLOADPATH=${SCRIPTPATH}/airootfs/etc/holoinstall
 #downloadendpoint=https://cd2.holoiso.ru.eu.org/holoiso-images
 downloadendpoint=http://109.164.107.149/holoiso/holoiso-images
 preferred_imgtype=".img.zst"
-endpoint=https://cd2.holoiso.ru.eu.org/holoiso-images/latest_${UPD_BRANCH}.releasemeta
+#endpoint=https://cd2.holoiso.ru.eu.org/holoiso-images/latest_${UPD_BRANCH}.releasemeta
+endpoint=http://109.164.107.149/holoiso/holoiso-images/latest_${UPD_BRANCH}.releasemeta
 curl -s "${endpoint}" > ${SCRIPTPATH}/buildinfo
 
 source ${SCRIPTPATH}/buildinfo
