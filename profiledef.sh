c1e68d507be585e346dc3196f13635a94a6c829a
xxcosita3czxx <kokosove18@gmail.com>
2026-01-01 16:16:12 +0100

    idk ill just leave it there + removing legacy support

--- profiledef.sh
+++ profiledef.sh
@@ -8,9 +8,10 @@ iso_application="Screaming in public restrooms part three"
 iso_version="InstallerBuild-$(date +%Y%m%d%H%M%S)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-x64.grub.esp'
-           'uefi-x64.grub.eltorito')
+bootmodes=(
+          'uefi-x64.grub.esp'
+          'uefi-x64.grub.eltorito'
+)
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
